66db61f0efbf295b059b8b3d19c4186f651cc111
Mohammad Frehat <mohammadfrehatx95@gmail.com>
2025-11-12 22:04:08 +0300

    Delete Dtos directory

--- Dtos/Auth/LoginDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HRMS2.Dtos.Auth
-{
-    public class LoginDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-
-    }
-}
--- Dtos/Departments/FilterDepartmentDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HRMS2.Dtos.Departments
-{
-    public class FilterDepartmentDto
-    {
-       public string? Name { get; set; }
-       public int? FloorNumber { get; set; }
-    }
-}
